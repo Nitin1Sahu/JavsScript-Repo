8ccafe7b7c56b666c85b21cbbf262e96e43669a9
Nitin <99113628+Nitin1Sahu@users.noreply.github.com>
2022-12-25 18:00:27 +0000

    Day2

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 111
+var accountEmail = "nitinsahu9899@gmail.com"
+let accountPassword = "213423"
+accountCity = "Delhi" // Prefer not to use
+
+// accountId = 12 // not allowed
+
+accountEmail = "sahunitin970@gmail.com"
+accountPassword = "1234"
+accountCity = "Bangaluru"
+console.log(accountId);
+/*
+Prefer not to use var
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
